8fbf9869293f2cd2db2c96b5a29d053fa144bd8b
Mike Ries <mries92@gmail.com>
2020-08-25 19:47:10 -0400

    Added item counters to menu

--- HoverStats/HoverStats.cs
+++ HoverStats/HoverStats.cs
@@ -3,16 +3,20 @@ using RoR2;
 using RoR2.UI;
 using System.Collections.ObjectModel;
 using R2API.Utils;
+using TMPro;
+using UnityEngine;
 
 namespace HoverStats
 {
-    [BepInPlugin("org.mries.hoverstats", "HoverStats", "0.0.1")]
+    [BepInPlugin("org.mries.hoverstats", "HoverStats", "1.0.0")]
     [BepInProcess("Risk of Rain 2.exe")]
     public class HoverStats : BaseUnityPlugin
     {
+        AssetBundle bundle;
         void Awake()
         {
             On.RoR2.UI.PickupPickerPanel.SetPickupOptions += SetPickupOptions;
+            bundle = AssetBundle.LoadFromMemory(Properties.Resources.bundle);
         }
 
         public void SetPickupOptions(On.RoR2.UI.PickupPickerPanel.orig_SetPickupOptions orig, RoR2.UI.PickupPickerPanel self, RoR2.PickupPickerController.Option[] options)
@@ -20,12 +24,18 @@ namespace HoverStats
             orig(self, options);
             var allocator = self.GetFieldValue<UIElementAllocator<MPButton>>("buttonAllocator");
             ReadOnlyCollection<MPButton> buttons = allocator.elements;
-            TooltipProvider provider = new TooltipProvider();
-
-            int i = 0;
-            foreach (var button in buttons)
+            LocalUser user = LocalUserManager.GetFirstLocalUser();
+            CharacterBody body = user.cachedBody;
+            Inventory inv = body.inventory;
+            for(int i = 0; i < buttons.Count; i++)
             {
+                MPButton button = buttons[i];
+
                 TooltipContent content = new TooltipContent();
+                var text_object = Instantiate(bundle.LoadAsset<GameObject>("ItemCountText"), button.transform);
+                var mesh = text_object.GetComponent<TextMeshProUGUI>();
+                text_object.GetComponent<RectTransform>().localPosition += new Vector3(20, -25);
+
                 var def = PickupCatalog.GetPickupDef(options[i].pickupIndex);
                 var idef = ItemCatalog.GetItemDef(def.itemIndex);
                 var edef = EquipmentCatalog.GetEquipmentDef(def.equipmentIndex);
@@ -34,16 +44,16 @@ namespace HoverStats
                     content.titleColor = def.darkColor;
                     content.titleToken = idef.nameToken;
                     content.bodyToken = idef.descriptionToken;
+                    mesh.SetText(inv.GetItemCount(def.itemIndex).ToString());
                 }
                 else
                 {
                     content.titleColor = def.darkColor;
                     content.titleToken = edef.nameToken;
                     content.bodyToken = edef.descriptionToken;
+                    mesh.SetText("");
                 }
-
                 button.gameObject.AddComponent<TooltipProvider>().SetContent(content);
-                i++;
             }
         }
     }
